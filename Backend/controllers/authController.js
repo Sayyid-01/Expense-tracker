f7c9b288b2d808fc58a81e10400533faffb4b489
sahil <sayyed@Anonymous.local>
2026-08-30 18:27:10 +0530

    Upgrading password to hash password

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-
+import bcrypt from "bcrypt";
 export const signup = async (req, res) => {
     try {
         const { name, email, password } = req.body;
@@ -12,11 +12,11 @@ export const signup = async (req, res) => {
                 error: "User already exists"
             });
         }
-
+        const hashPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
             name,
             email,
-            password
+            password: hashPassword
         });
 
         res.status(201).json({
@@ -42,11 +42,18 @@ export const login = async(req,res) =>{
             error: "User not found"
         });
     }
-    if(user.password !== password){
-        return res.status(401).json({
-            error: "User not authorized"
-        });
-    }
+    bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                error: "Error comparing the passwords"
+            });
+        }
+        if (!result) {
+            return res.status(401).json({
+                error: "Invalid password"
+            });
+        }
+    })
     res.status(200).json({
         message: "Login successful",
         user
